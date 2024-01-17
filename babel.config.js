aa2139de9e8a72300eb8492ba509206ff31ff1a3
Oded <oded@permit.io>
2024-01-17 17:31:29 +0200

    tests

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,13 @@
 module.exports = {
-    presets: [
-      ['@babel/preset-env', {targets: {node: 'current'}}],
-      '@babel/preset-typescript',
-    ],
-  };
+  presets: [
+    ['@babel/preset-env', {
+      targets: "> 0.25%, not dead"
+    }],
+    '@babel/preset-typescript',
+  ],
+  "plugins": [
+    ["@babel/plugin-transform-runtime", {
+      "regenerator": true
+    }],
+
+};
